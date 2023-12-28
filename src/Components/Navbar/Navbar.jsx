6fa471b1cd4843144a2291e7c0e5acc8ca9fa3b3
Mandeep Singh <126713166+mannii2001@users.noreply.github.com>
2023-12-28 13:20:06 +0530

    break Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -48,7 +48,7 @@ const Navbar = () => {
         </h1>
       </div>
       <div className="centerMenu">
-        <p className="menuText" onClick={navigateHome}>lemon in the Home</p>
+        <p className="menuText" onClick={navigateHome}>break in the Home</p>
         <p className="menuText" onClick={navigateClasses}>Classes</p>
         <p className="menuText" onClick={navigateAboutus}>About Us</p>
         <p className="menuText" onClick={navigateContactus}>Contact Us</p>
